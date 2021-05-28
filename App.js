cc934571f0dbe08d3c0fd228c409f73fbd199adc
Dimitar Filipov <dfilipov94@gmail.com>
2021-05-28 17:26:56 +0300

    Update firebase config

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
+
 import { ThemeProvider } from "styled-components/native";
 import * as firebase from "firebase";
 
 require("firebase/functions");
+
+const firebaseConfig = require("./firebaseconfig.json");
 
 import { theme } from "./src/infrastructure/theme";
 
@@ -16,14 +19,6 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { Navigation } from "./src/infrastructure/navigation";
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAv6aSckZ-3M3xhvIgTRE-Q0qnkA5fhkco",
-    authDomain: "mealstogo-3aa0d.firebaseapp.com",
-    projectId: "mealstogo-3aa0d",
-    storageBucket: "mealstogo-3aa0d.appspot.com",
-    messagingSenderId: "771889417911",
-    appId: "1:771889417911:web:4f2a0ad8701200bc818732",
-};
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
